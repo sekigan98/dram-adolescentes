9a8e07be57a57e97b8514685b5254bc1f33010f4
sekigan98 <sekigan98@gmail.com>
2025-12-03 00:48:25 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,4 @@
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   return [
     {
       url: 'https://adolecencia-para-padres.vercel.app',
