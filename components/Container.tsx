136248f6fd34a4bee93f3cecb825f46120c8cd35
sekigan98 <sekigan98@gmail.com>
2025-12-04 21:13:31 -0300

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,15 +1,25 @@
 import clsx from "clsx";
+import { ElementType, ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+  as?: ElementType; // 👈 permite cambiar el tag (div, section, main, etc.)
+  [key: string]: any; // 👈 permite pasar props adicionales (id, aria-label, etc.)
+}
 
 export default function Container({
   children,
-  className
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
+  className,
+  as: Tag = "div",
+  ...props
+}: ContainerProps) {
   return (
-    <div className={clsx("container-padding max-w-6xl mx-auto", className)}>
+    <Tag
+      className={clsx("container-padding max-w-6xl mx-auto", className)}
+      {...props}
+    >
       {children}
-    </div>
+    </Tag>
   );
 }
